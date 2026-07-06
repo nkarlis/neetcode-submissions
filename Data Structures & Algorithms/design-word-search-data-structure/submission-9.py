8989118e050857c1c2e5be81084638c0eb0007a5
nkarlis <94298171+nkarlis@users.noreply.github.com>
2026-07-06 21:56:30 +0300

    Add: design-word-search-data-structure - submission-9

---
+++ Data Structures & Algorithms/design-word-search-data-structure/submission-9.py
@@ -0,0 +1,40 @@
+class TrieNode:
+
+    def __init__(self):
+        self.children = {}
+        self.word = False
+
+class WordDictionary:
+
+    def __init__(self):
+        self.root = TrieNode()
+        
+
+    def addWord(self, word: str) -> None:
+        cur = self.root
+        for c in word:
+            if c not in cur.children:
+                cur.children[c] = TrieNode()
+            cur = cur.children[c]
+        cur.word = True
+        
+
+    def search(self, word: str) -> bool:
+        def dfs(j, root):
+            curr = root
+            for i in range(j, len(word)):
+                c = word[i]
+                if c == ".":
+                    for child in curr.children.values():
+                        if dfs(i + 1, child):
+                            return True
+                    return False
+                else:
+                    if c not in curr.children:
+                        return False
+                    curr = curr.children[c]
+            return curr.word
+
+
+        return dfs(0, self.root)
+        
