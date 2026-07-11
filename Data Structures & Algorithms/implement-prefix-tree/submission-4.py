35b3d172748555905cf4461efeeb99290284eb63
nkarlis <94298171+nkarlis@users.noreply.github.com>
2026-07-11 16:33:47 +0300

    Add: implement-prefix-tree - submission-4

---
+++ Data Structures & Algorithms/implement-prefix-tree/submission-4.py
@@ -0,0 +1,41 @@
+class PrefixTreeNode:
+
+    def __init__(self):
+        self.end = False
+        self.children = {}
+
+
+class PrefixTree:
+
+    def __init__(self):
+        self.root = PrefixTreeNode()
+    
+        
+
+    def insert(self, word: str) -> None:
+        cur = self.root
+        for c in word:
+            if c not in cur.children:
+                cur.children[c] = PrefixTreeNode()
+            cur = cur.children[c]
+        cur.end = True
+
+
+    def search(self, word: str) -> bool:
+        cur = self.root
+        for c in word:
+            if c not in cur.children:
+                return False
+            cur = cur.children[c]
+        return cur.end
+        
+
+    def startsWith(self, prefix: str) -> bool:
+        cur = self.root
+        for c in prefix:
+            if c not in cur.children:
+                return False
+            cur = cur.children[c]
+        return True
+        
+        
