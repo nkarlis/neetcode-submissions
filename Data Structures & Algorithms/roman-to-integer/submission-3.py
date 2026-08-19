06840af09126ea5122158c7700bec9d1f375d1f5
nkarlis <94298171+nkarlis@users.noreply.github.com>
2026-08-19 19:36:17 +0300

    Add: roman-to-integer - submission-3

---
+++ Data Structures & Algorithms/roman-to-integer/submission-3.py
@@ -0,0 +1,18 @@
+class Solution:
+    def romanToInt(self, s: str) -> int:
+        symbolToVal = { "I" : 1,
+                        "V" : 5,
+                        "X" : 10,
+                        "L" : 50,
+                        "C" : 100,
+                        "D" : 500,
+                        "M" : 1000
+                        }
+        res = 0
+        for i in range(len(s)):
+            if (i + 1 < len(s) and 
+                symbolToVal[s[i]] < symbolToVal[s[i + 1]]):
+                res -= symbolToVal[s[i]]
+            else:
+                res += symbolToVal[s[i]]
+        return res
