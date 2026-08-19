8f75728461d8d6d91980cf19237005da7589a160
nkarlis <94298171+nkarlis@users.noreply.github.com>
2026-08-19 16:47:31 +0300

    Add: reverse-integer - submission-4

---
+++ Data Structures & Algorithms/reverse-integer/submission-4.py
@@ -0,0 +1,16 @@
+class Solution:
+    def reverse(self, x: int) -> int:
+        MIN, MAX = -2**31, 2**31 -1
+        sign = 1
+        if x < 0:
+            sign = -1
+        x = abs(x)
+        res = 0
+        while x:
+            digit = (x % 10)
+            x //= 10
+            if res > (MAX - digit) // 10:
+                return 0
+            res = (res * 10) + digit
+        return sign * res
+           
