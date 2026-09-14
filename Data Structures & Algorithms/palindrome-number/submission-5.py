74136458319dbc40594a7bd7c234999fb3979ca0
nkarlis <94298171+nkarlis@users.noreply.github.com>
2026-09-14 20:30:29 +0300

    Add: palindrome-number - submission-5

---
+++ Data Structures & Algorithms/palindrome-number/submission-5.py
@@ -0,0 +1,10 @@
+class Solution:
+    def isPalindrome(self, x: int) -> bool:
+        if x < 0:
+            return False
+        res = 0
+        temp = x
+        while temp:
+            res = (res * 10) + temp % 10
+            temp //= 10
+        return res == x
